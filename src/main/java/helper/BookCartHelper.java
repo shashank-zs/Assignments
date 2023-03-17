469c13be69fdd4e727f1fd43ffe85a85717e1f55
shashank-zs <zs@92146>
2023-03-17 17:40:55 +0530

    changes done

--- src/main/java/helper/BookCartHelper.java
+++ src/main/java/helper/BookCartHelper.java
@@ -5,9 +5,10 @@ import io.restassured.response.Response;
 import junit.framework.Assert;
 
 public class BookCartHelper {
-    public static void getBookStoreAssertions(Response response, Object bookId){
+    public  static void getBookStoreAssertions(Response response, Object bookId){
         JsonPath jsonpath = response.jsonPath();
         Integer responseBookID= jsonpath.getInt("[0].book.bookId");
+
         if (bookId!=null && responseBookID !=null){
             Assert.assertEquals(bookId,String.valueOf(responseBookID) );
         }
